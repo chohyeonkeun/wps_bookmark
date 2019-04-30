fae831f1995659e8315f9130ccf771301ce68c7f
habitance <positipman@gmail.com>
2019-04-30 15:13:15 +0900

    second

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('bookmark/', include('bookmark.urls')),
+    path('', include('bookmark.urls')),
     path('admin/', admin.site.urls),
 ]
 
